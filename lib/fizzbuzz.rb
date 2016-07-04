40a770beac6e33fbec3574b9362ec00e87ec646b
Adam <adamjbrowner@mgmail.com>
2016-07-04 17:06:49 +0100

    Number one

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,18 @@
+def bythree(x)
+  true if x % 3 == 0
+end
+
+def byfive(x)
+  true if x % 5 == 0
+end
+
+def fizzbuzz(x)
+  if bythree(x) and byfive(x)
+    'fizzbuzz'
+  elsif bythree(x) and !byfive(x)
+    'fizz'
+  elsif byfive(x) and !bythree(x)
+    'buzz'
+  end
+
+end
