b887e86d4cfe030364e8ae72a96fcf368329e4d7
Adam <adamjbrowner@mgmail.com>
2016-07-05 08:05:24 +0100

    number 9

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,17 +1,17 @@
 def three(x)
-  x % 3 == 0
+  true if x % 3 == 0
 end
 
 def five(x)
-  x % 5 == 0
+  true if x % 5 ==0
 end
 
 def fizzbuzz(x)
-  if five(x) and three(x)
+  if three(x) and five (x)
     'fizzbuzz'
-  elsif five(x) and !three(x)
-    'buzz'
   elsif three(x) and !five(x)
     'fizz'
+  elsif five(x) and !three(x)
+    'buzz'
   end
 end
